1c922e041f15a6fa5a24cbc155c0b073b1712548
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-05-10 16:56:45 +0200

    Create empty project

---
+++ SingleCombineCall/SingleCombineCall/AppDelegate/SingleCombineCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingleCombineCallApp.swift
+//  SingleCombineCall
+//
+//  Created by Matthias Zarzecki on 10.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SingleCombineCallApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
