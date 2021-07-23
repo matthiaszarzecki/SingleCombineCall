1585584eb0359747daeb5b7484e82a560c42e50e
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2021-07-23 15:58:51 +0200

    Remove unused elements

--- SingleCombineCall/SingleCombineCall/Api/Api.swift
+++ SingleCombineCall/SingleCombineCall/Api/Api.swift
@@ -8,22 +8,14 @@
 import Combine
 import Foundation
 
-/// Contains functions to request and
-/// receive data from the ApiOfIceAndFire.
 struct Api {
-  /// Gets 30 ASOIAF Houses.
   static func getHouses() -> AnyPublisher<[HouseBasic], Error> {
     let url = URL(string: "https://www.anapioficeandfire.com/api/houses?page=0&pageSize=30")!
     var request = URLRequest(url: url)
     request.httpMethod = "GET"
-
+    
     return URLSession.shared
-     .dataTaskPublisher(for: request)
-      .handleEvents(
-        receiveOutput: {
-          $0.data.printJsonFromData()
-        }
-      )
+      .dataTaskPublisher(for: request)
       .tryMap {
         return try JSONDecoder().decode(
           [HouseBasic].self,
